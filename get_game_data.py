04cfbdf916f5f203c0d4035c4d37f43130015fc4
DragoBJJ <j.pawelski.it@gmail.com>
2023-05-25 09:46:23 +0200

    Copy data and create new directory from them

--- get_game_data.py
+++ get_game_data.py
@@ -19,19 +19,38 @@ def find_all_game_paths(source):
     return game_paths
 
 
+def get_name_from_paths(paths, to_strip):
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_strip, "")
+        new_names.append(new_dir_name)
+    return new_names
+
+
 def create_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
+
+
+def copy_and_overwrite(source, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(source, dest)
 
 
 def main(source, target):
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
     target_path = os.path.join(cwd, target)
-    print(f"Your location: {source_path}")
+    create_dir(target)
 
     game_paths = find_all_game_paths(source_path)
-    create_dir(target)
+    new_game_dir = get_name_from_paths(game_paths, "_game")
+
+    for src, dest in zip(game_paths, new_game_dir):
+        dest_path = os.path.join(target_path, dest)
+        copy_and_overwrite(src, dest_path)
 
 
 if __name__ == "__main__":

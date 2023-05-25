cb94198d63da54b49a3d635e53b51dac0628d567
DragoBJJ <j.pawelski.it@gmail.com>
2023-05-25 10:29:24 +0200

    create compile command function

--- get_game_data.py
+++ get_game_data.py
@@ -5,6 +5,8 @@ from subprocess import PIPE, run
 import sys
 
 GAME_DIR_PATTERN = "game"
+GAME_CODE_EXTENSION = ".go"
+GAME_COMPILE_COMMAND = ["go", "build"]
 
 
 def find_all_game_paths(source):
@@ -49,18 +51,45 @@ def make_json_metadata_file(path, game_dirs):
         json.dump(data, f)
 
 
+def compile_game_code(source):
+    code_file_name = None
+    for root, dirs, files in os.walk(source):
+        for file in files:
+            if file.endswith(GAME_CODE_EXTENSION):
+                code_file_name = file
+                break
+        break
+
+    if code_file_name is None:
+        return
+
+    command = GAME_COMPILE_COMMAND + [code_file_name]
+    run_command(command, source)
+
+
+def run_command(command, path):
+    cwd = os.getcwd()
+    os.chdir(path)
+    result = run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    print(f"Result of execute command: {result}")
+
+    os.chdir(cwd)
+
+
 def main(source, target):
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
     target_path = os.path.join(cwd, target)
-    create_dir(target)
 
     game_paths = find_all_game_paths(source_path)
     new_game_dirs = get_name_from_paths(game_paths, "_game")
 
+    create_dir(target)
+
     for src, dest in zip(game_paths, new_game_dirs):
         dest_path = os.path.join(target_path, dest)
         copy_and_overwrite(src, dest_path)
+        compile_game_code(dest_path)
 
     json_path = os.path.join(target_path, "metadata.json")
     make_json_metadata_file(json_path, new_game_dirs)
